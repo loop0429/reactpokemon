aff70271e07110de749e6fe745a8cfedc68e055d
永岡史哉 <model.f.n@gmail.com>
2020-01-14 23:16:13 +0900

    spritesmith導入

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import App from 'next/app'
 import '../css/tailwind.css'
-import '../css/sprite.css'
+import '../css/sprite1.css'
+import '../css/sprite2.css'
+import '../css/sprite3.css'
+import '../css/sprite4.css'
+import '../css/sprite5.css'
+import '../css/sprite6.css'
+import '../css/sprite7.css'
+import '../css/sprite8.css'
+import '../css/sprite9.css'
+import '../css/sprite10.css'
+import '../css/sprite11.css'
+import '../css/sprite12.css'
+import '../css/sprite13.css'
+import '../css/sprite14.css'
 
 class MyApp extends App {
   render() {
