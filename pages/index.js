00a86f1bc0d74308eb20f604412b48976eaecb38
永岡史哉 <model.f.n@gmail.com>
2020-01-20 22:50:15 +0900

    不要なの削除

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,11 @@ import { withRedux } from '../lib/redux'
 import Header from '../components/header'
 import Sidebar from '../components/sidebar'
 import Modal from '../components/modal'
-import { showFeatureModal } from '../actions'
-import { useDispatch } from 'react-redux'
-
-const useModal = () => {
-  const dispatch = useDispatch()
-
-  const open = () => {
-    dispatch(showFeatureModal('FEATURE'))
-  }
-
-  return { open }
-}
 
 const IndexPage = () => {
-  const { open } = useModal()
   return (
     <>
       <Header />
-      <button type="button" onClick={open}>hoge</button>
       <Modal />
     </>
   )
