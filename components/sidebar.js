7e7e6660d0e056210145bba974807529deff9811
Fumiya Nagaoka <nagaoka@goq.co.jp>
2020-01-21 08:56:00 +0900

    サイドバーのトグルを動く様にした

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { toggleSidebar } from '../actions'
 
 const useSidebar = () => {
   const isOpenSidebar = useSelector((state) => state.sidebarReducer.isOpenSidebar)
+  const dispatch = useDispatch()
+
+  const handleOverlayClick = () => {
+    dispatch(toggleSidebar())
+  }
+
+  return { isOpenSidebar, handleOverlayClick }
 }
 
 const Sidebar = () => {
+  const { isOpenSidebar, handleOverlayClick } = useSidebar()
+  let sidebarClass = 'sidebar'
+  if (isOpenSidebar === true) {
+    sidebarClass = `${sidebarClass} is-open`
+  }
   return (
-    <div className="sidebar is-open">
-      <div className="sidebar__overlay" />
+    <div className={sidebarClass}>
+      <div className="sidebar__overlay" onClick={handleOverlayClick} />
       <div className="sidebar__wrapper">
         <dl className="m-0">
-          <dt className="bg-gray-200">絞り込み検索</dt>
+          <dt className="bg-gray-200 p-2">絞り込み検索</dt>
         </dl>
       </div>
       <style jsx>{`
@@ -20,6 +33,7 @@ const Sidebar = () => {
           left: -250px;
           top: 0;
           overflow-y: scroll;
+          width: 250px;
           height: 100%;
           background: #FFF;
           transition: all .5s;
